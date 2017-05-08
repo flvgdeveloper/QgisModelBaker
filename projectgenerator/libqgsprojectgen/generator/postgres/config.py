9e69f2d7854a27c74fb302395c9b349bfabb2412
gacarrillor <geotux_tuxman@linuxmail.org>
2017-05-08 14:45:32 -0500

    Add table names to ignore

--- projectgenerator/libqgsprojectgen/generator/postgres/config.py
+++ projectgenerator/libqgsprojectgen/generator/postgres/config.py
@@ -14,12 +14,22 @@ IGNORED_TABLES = [
     't_ili2db_inheritance',
     't_ili2db_model',
     't_ili2db_classname',
-    't_ili2db_import_basket'
+    't_ili2db_import_basket',
+    'oid',
+    'fraction',
+    'dq_element',
+    'ci_contact',
+    'ci_responsibleparty',
+    'la_partyroletype'
 ]
 
 IGNORED_FIELDNAMES = [
     't_id',
-    't_seq'
+    't_seq',
+    'beginlifespanversion',
+    'endlifespanversion',
+    'sharecheck',
+    'timespec'
 ]
 
 READONLY_FIELDNAMES = [
